b2f9d64cb138612401f95416c22b07570858f1cd
Mogball <jeffniu22@gmail.com>
2018-07-09 11:44:44 -0500

    Created AVR-test for pkg-list

--- tests/project-pkg/pkg-list/include/pkg-list.h
+++ tests/project-pkg/pkg-list/include/pkg-list.h
@@ -4,14 +4,17 @@
 namespace wio {
     class dynamic_stack {
     public:
-        explicit dynamic_stack(int size);
+        explicit dynamic_stack(int capacity);
         ~dynamic_stack();
 
         void append(int val);
         int pop();
+        int size() const;
+        int capacity() const;
 
     private:
         int m_size;
+        int m_capacity;
         int *m_data;
     };
 }
--- tests/project-pkg/pkg-list/src/impl.cpp
+++ tests/project-pkg/pkg-list/src/impl.cpp
@@ -3,17 +3,27 @@
 
 using namespace wio;
 
-dynamic_stack::dynamic_stack(int size) :
+dynamic_stack::dynamic_stack(int capacity) :
     m_size(0),
-    m_data(static_cast<int *>(stack_alloc(size))) {}
+    m_capacity(capacity),
+    m_data(static_cast<int *>(stack_alloc(capacity))) {}
 
 dynamic_stack::~dynamic_stack()
 { stack_reset(); }
 
-void dynamic_stack::append(int val)
-{ m_data[m_size++] = val; }
+void dynamic_stack::append(int val) {
+    if (m_size >= m_capacity) { return; }
+    m_data[m_size++] = val;
+}
 
 int dynamic_stack::pop() {
+    if (m_size == 0) { return 0; }
     --m_size;
     return m_data[m_size];
 }
+
+int dynamic_stack::size() const
+{ return m_size; }
+
+int dynamic_stack::capacity() const
+{ return m_capacity; }
